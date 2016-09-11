e08eb3ea40eacc606393389c548d66b1770b48ae
Gl1tchHub <FaZeClanDestroyer@outlook.com>
2016-09-11 18:30:29 -0400

    Create DankMemes.js

---
+++ DankMemes.js
@@ -0,0 +1,11 @@
+$.getJSON("https://api.twitch.tv/kraken/streams/" + "evanbear1", function(channel) {
+   if (channel["stream"] == null) {
+
+$("#WikiaRailWrapper").prepend("Fuck")
+
+   } else {
+
+$("#WikiaRailWrapper").prepend("NOICE")
+   }
+
+});
