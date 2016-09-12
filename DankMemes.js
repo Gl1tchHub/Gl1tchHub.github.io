59612350ad1b9070c23d98656a62180c0a8ead85
Gl1tchHub <FaZeClanDestroyer@outlook.com>
2016-09-12 13:03:17 -0400

    Update DankMemes.js

--- DankMemes.js
+++ DankMemes.js
@@ -1,14 +1,12 @@
+window.setInterval(function(){
 $.getJSON("https://api.twitch.tv/kraken/streams/" + "evanbear1", function(channel) {
    if (channel["stream"] == null) {
-
 $("#WikiaRailWrapper").prepend("Fuck")
-
    } else {
-
-$("#WikiaRailWrapper").prepend("NOICE")
    }
-
 });
+}, 5000);
+
 
 $( document ).ready(function() {
 $("#WikiHeader").append("<div style="color:#EEEEEE;background-color:#000000;text-align:center;">Follow Berezaa on Twitter @Berezagames</div>");
